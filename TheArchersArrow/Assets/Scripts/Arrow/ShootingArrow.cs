9c305083f2e0ba8e419184752bdad1e98dc435f9
MrNiceGuy12312 <52978365+MrNiceGuy12312@users.noreply.github.com>
2020-07-08 11:18:56 -0400

    Max Ammo
    added a maximum of 3 arrows to the game

--- TheArchersArrow/Assets/Scripts/Arrow/ShootingArrow.cs
+++ TheArchersArrow/Assets/Scripts/Arrow/ShootingArrow.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShootingArrow : MonoBehaviour
 {
+
+    public int maxAmmo = 3;
+    public int currentAmmo = -1;
 
     public float speed;
 
@@ -12,20 +16,30 @@ public class ShootingArrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+           if (currentAmmo == -1)
+        {
+            currentAmmo = maxAmmo;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+      if(currentAmmo <= 0)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Shoot();
         }
+
     }
 
     void Shoot()
     {
+        currentAmmo--;
         GameObject ArrownIns = Instantiate(Arrow, transform.position, transform.rotation);
         //add the speed the arrow shoots at
 
