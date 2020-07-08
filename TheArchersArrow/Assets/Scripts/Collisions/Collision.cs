d4a6430c3c856b9995f88df8cd2ce430d5290f37
MrNiceGuy12312 <52978365+MrNiceGuy12312@users.noreply.github.com>
2020-07-08 15:39:25 -0400

    Fixed the collisions

---
+++ TheArchersArrow/Assets/Scripts/Collisions/Collision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{         
+    [SerializeField] private string newLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Target")
+        {
+
+            Destroy(col.gameObject);
+            Score.scoreValue += 50;
+            SceneManager.LoadScene(newLevel);
+        }
+        
+        if (col.gameObject.tag == "Arrow")
+        {
+
+            Destroy(col.gameObject);
+
+        }
+
+        if (col.gameObject.tag == "Blockade")
+        {
+            Score.scoreValue += 20;
+            Destroy(col.gameObject);
+
+        }
+    }
+ }
